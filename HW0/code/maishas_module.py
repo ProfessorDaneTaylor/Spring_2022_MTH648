34fc8dcf6e18dfaca65c3f4232fb7b04e6f8daa8
Sejunti <96703735+Sejunti@users.noreply.github.com>
2022-02-09 21:00:37 -0500

    maishas_module

---
+++ HW0/code/maishas_module.py
@@ -0,0 +1,26 @@
+# Maisha's module contains Maisha's functions
+
+import numpy as np
+
+def multiplication_of_two_numbers(x,y):
+	"""
+    Maisha's first function.
+    Args:
+        x (float): Some number.
+        y (float): Some number.
+    Returns:
+        float: Returns :math:`m=x*y`
+    """
+
+	return x*y
+
+def cube_of(x):
+	"""
+    Maisha's second function.
+    Args:
+        x (float): Some number.
+    Returns:
+        float: Returns :math:`y=x**3`
+    """
+    
+	return x**3
